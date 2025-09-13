2eb02a860d0ea827c736fbe0c8aaf5435754c818
omokhepe <fimobu@gmail.com>
2025-09-13 16:44:14 +0800

    First commit

---
+++ public/assets/images/index.js
@@ -0,0 +1,17 @@
+import auth_illustration from './illustration-authentication.svg';
+import home_icon from './icon-nav-overview.svg';
+import transaction_icon from './icon-nav-transactions.svg';
+import budget_icon from './icon-nav-budgets.svg';
+import pot_icon from './icon-nav-pots.svg';
+import bill_icon from './icon-nav-recurring-bills.svg';
+import minimize_icon from './icon-minimize-menu.svg';
+
+export {
+  auth_illustration,
+  home_icon,
+  transaction_icon,
+  budget_icon,
+  pot_icon,
+  bill_icon,
+  minimize_icon,
+};
